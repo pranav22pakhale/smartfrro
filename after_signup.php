224f4e2979868c22a83cbc33d7d4ed1891af4416
Jaydeep245 <jaydeepranoliya24@gmail.com>
2018-03-20 14:42:28 +0530

    after registration and signup page

---
+++ after_signup.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Bootstrap Example</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+  <link href='http://fonts.googleapis.com/css?family=Titillium+Web:400,300,600' rel='stylesheet' type='text/css'>
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/5.0.0/normalize.min.css">
+  <link rel="stylesheet" href="css/style.css">
+
+
+<style>
+.well-lg{
+background-color: #263238 !important;
+width: 50%;
+height: 500px;
+text-decoration-color: white;
+position: absolute;
+transform: translateY(20%);
+margin-left:25%;
+}
+
+.alert-info-alt { 
+	border-color: #B4E1E4;
+	background: #81c7e1;
+	color: #fff; 
+}
+
+
+</style>
+</head>
+<body>
+
+	<center>
+   	<div class="well well-lg">
+
+   		<h1 style="color: white; font-size: 50px; font-family: 'Titillium Web', sans-serif;">Thank you for Sign Up!</h1>
+
+        <div class="alert alert-success">
+  			<strong style="font-size: 30px;">You have signed up successfully !</strong>
+		</div>
+		
+			<div class="alert alert-danger" style="font-size: 25px;">
+	  			<strong style="font-size: 25px;"> Account is unverified, please confirm your email by clicking on the email link!</strong>
+			</div>
+    </div>
+</center>
+
+
+
+
+</div>
+
+
+</body>
+</html>
