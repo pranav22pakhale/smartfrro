b9bf102ddb1f228bce1405ad145f27fb8693b8c8
Akshay Mane <mane.akshay1997@gmail.com>
2018-03-30 17:45:36 +0530

    changes

--- purpose.php
+++ purpose.php
@@ -2,8 +2,8 @@
 	if(!isset($_SESSION)){
 		session_start();
 	}
-	if(!isset($_SESSION['user_logged'])){
-		header("Location: login_user.php");
+	if(!isset($_SESSION['frro_logged'])){
+		header("Location: loginFRRO.php");
 	}
 
 	if(!isset($_GET['dept'])){
@@ -262,11 +262,15 @@ require_once 'head.php';
         </tbody>
     </table>
 </div>
-</div>
-<label></label>
-<div style="width: 40%">
+<label></label>				
+<div id="canvas-holder" class="col-md-5">
+			<canvas id="chart-area" width="300" height="300"/>
+		</div>
+
+<div  class="col-md-5">
 			<center><canvas id="canvas" height="450" width="600"></canvas></center>
 		</div>
+</div>
 </div>
 
 <footer class="main-footer">	
@@ -302,6 +306,43 @@ require_once 'head.php';
 </script>
 
 	<script>
+
+		var pieData = [
+				{
+					value: 300,
+					color:"#F7464A",
+					highlight: "#FF5A5E",
+					label: "Red"
+				},
+				{
+					value: 50,
+					color: "#46BFBD",
+					highlight: "#5AD3D1",
+					label: "Green"
+				},
+				{
+					value: 100,
+					color: "#FDB45C",
+					highlight: "#FFC870",
+					label: "Yellow"
+				},
+				{
+					value: 40,
+					color: "#949FB1",
+					highlight: "#A8B3C5",
+					label: "Grey"
+				},
+				{
+					value: 120,
+					color: "#4D5360",
+					highlight: "#616774",
+					label: "Dark Grey"
+				}
+
+			];
+			var ctx1 = document.getElementById("chart-area").getContext("2d");
+			window.myPie = new Chart(ctx1).Pie(pieData);
+
 	var randomScalingFactor = function(){ return Math.round(Math.random()*5)};
 
 	var barChartData = {
